bddfc38d0e66616faa8e3db560752f862754ee1c
Campos <55212557+juan977@users.noreply.github.com>
2022-07-18 20:49:59 -0500

    BIENVENIDO A PY

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 const app = require('./app')
 
-const express = require('express');
-
 const PORT = process.env.PORT || 3000;
 app.listen(3000, () => {
   console.log(`Server running on port ${PORT}`);
